4a7ffb91b5941d59bfedc73aa298f2850ffa4dde
Nathan Gordon <gordonnl@gmail.com>
2019-06-14 21:29:12 +0200

    update attrib location order string to use getAttribLocation

--- src/core/Program.js
+++ src/core/Program.js
@@ -1,7 +1,6 @@
 // TODO: upload empty texture if null ? maybe not
 // TODO: upload identity matrix if null ?
 // TODO: sampler Cube
-// TODO: is aIndex ever not equal to gl.getAttribLocation
 
 let ID = 0;
 
@@ -97,14 +96,15 @@ export class Program {
 
         // Get active attribute locations
         this.attributeLocations = new Map();
-        this.attributeOrder = '';
-        let numAttribs = gl.getProgramParameter(this.program, gl.ACTIVE_ATTRIBUTES);
+        const locations = []; 
+        const numAttribs = gl.getProgramParameter(this.program, gl.ACTIVE_ATTRIBUTES);
         for (let aIndex = 0; aIndex < numAttribs; aIndex++) {
-            let attribute = gl.getActiveAttrib(this.program, aIndex);
-            // this.attributeLocations.set(attribute.name, gl.getAttribLocation(this.program, attribute.name));
-            this.attributeLocations.set(attribute.name, aIndex);
-            this.attributeOrder += attribute.name;
+            const attribute = gl.getActiveAttrib(this.program, aIndex);
+            const location = gl.getAttribLocation(this.program, attribute.name);
+            locations[location] = attribute.name;
+            this.attributeLocations.set(attribute.name, location);
         }
+        this.attributeOrder = locations.join('');
     }
 
     setBlendFunc(src, dst, srcAlpha, dstAlpha) {
