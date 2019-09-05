9e4e94a1cd6a5f1acf8efe6589e836bc9914489f
Terkel Gjervig <terkel@terkel.com>
2019-09-05 14:11:59 -0400

    Add unpkg

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,8 @@ export default [
 		input: 'src/index.js',
 		output: [
 			{ file: pkg.main, format: 'cjs' },
-			{ file: pkg.module, format: 'es' }
+      { file: pkg.module, format: 'es' },
+      { file: pkg.browser, format: 'umd', name: 'ogl' }
 		]
 	}
 ];
