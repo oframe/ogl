3ac8d72c8a5723526a9dc4c835c34e19d684e4a7
Terkel <terkel@terkel.com>
2019-09-05 14:36:39 -0400

    Add simple build step (#19)
    * add simple build step with unpkg

---
+++ rollup.config.js
@@ -0,0 +1,10 @@
+import pkg from './package.json';
+
+export default [{
+  input: 'src/index.js',
+  output: [
+    { file: pkg.main, format: 'cjs' },
+    { file: pkg.module, format: 'es' },
+    { file: pkg.browser, format: 'umd', name: 'ogl' }
+  ]
+}];
