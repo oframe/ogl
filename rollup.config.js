f8d63908524f557379d09d2e5c1f2087471f8d31
Nathan Gordon <gordonnl@gmail.com>
2019-09-15 18:34:36 +0200

    Update dist to allow tree shaking with Webpack

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import pkg from './package.json';
 export default [{
   input: 'src/index.js',
   output: [
-    { file: pkg.main, format: 'cjs' },
+    { file: pkg.browser, format: 'cjs' },
     { file: pkg.module, format: 'es' },
     { file: pkg.unpkg, format: 'umd', name: 'ogl' }
   ]
