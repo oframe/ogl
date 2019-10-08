9f1a704232de35b44a08ca1f24e273f89f680c46
Nathan Gordon <gordonnl@gmail.com>
2019-10-08 15:41:36 +0200

    formatting

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,10 @@
 import pkg from './package.json';
 
 export default [{
-  input: 'src/index.js',
-  output: [
-    { file: pkg.browser, format: 'cjs' },
-    { file: pkg.module, format: 'es' },
-    { file: pkg.unpkg, format: 'umd', name: 'ogl' }
-  ]
+    input: 'src/index.js',
+    output: [
+        { file: pkg.browser, format: 'cjs' },
+        { file: pkg.module, format: 'es' },
+        { file: pkg.unpkg, format: 'umd', name: 'ogl' },
+    ],
 }];
