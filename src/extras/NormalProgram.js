45b917dae83758315961b87b5b72f8d07c7d31c2
Nathan Gordon <gordonnl@gmail.com>
2019-10-08 22:31:57 +0200

    added Shadow extra class

--- src/extras/NormalProgram.js
+++ src/extras/NormalProgram.js
@@ -1,34 +1,34 @@
 import {Program} from '../core/Program.js';
 
 const vertex = `
-precision highp float;
-precision highp int;
+    precision highp float;
+    precision highp int;
 
-attribute vec3 position;
-attribute vec3 normal;
+    attribute vec3 position;
+    attribute vec3 normal;
 
-uniform mat3 normalMatrix;
-uniform mat4 modelViewMatrix;
-uniform mat4 projectionMatrix;
+    uniform mat3 normalMatrix;
+    uniform mat4 modelViewMatrix;
+    uniform mat4 projectionMatrix;
 
-varying vec3 vNormal;
+    varying vec3 vNormal;
 
-void main() {
-    vNormal = normalize(normalMatrix * normal);
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-}
+    void main() {
+        vNormal = normalize(normalMatrix * normal);
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    }
 `;
 
 const fragment = `
-precision highp float;
-precision highp int;
+    precision highp float;
+    precision highp int;
 
-varying vec3 vNormal;
+    varying vec3 vNormal;
 
-void main() {
-    gl_FragColor.rgb = normalize(vNormal);
-    gl_FragColor.a = 1.0;
-}
+    void main() {
+        gl_FragColor.rgb = normalize(vNormal);
+        gl_FragColor.a = 1.0;
+    }
 `;
 
 export function NormalProgram(gl) {
---
+++ src/extras/Shadow.js
@@ -0,0 +1,124 @@
+import {Camera} from '../core/Camera.js';
+import {Program} from '../core/Program.js';
+import {RenderTarget} from '../core/RenderTarget.js';
+
+export class Shadow {
+    constructor(gl, {
+        light = new Camera(gl),
+        width = 1024,
+        height = width,
+    }) {
+        this.gl = gl;
+
+        this.light = light;
+        
+        this.target = new RenderTarget(gl, {width, height});
+
+        this.depthProgram = new Program(gl, {
+            vertex: defaultVertex,
+            fragment: defaultFragment,
+            cullFace: null,
+        });
+
+        this.castMeshes = [];
+    }
+
+    add({
+        mesh,
+        receive = true,
+        cast = true,
+        vertex = defaultVertex,
+        fragment = defaultFragment,
+        uniformProjection = 'shadowProjectionMatrix',
+        uniformView = 'shadowViewMatrix',
+        uniformTexture = 'tShadow',
+    }) {
+
+        // Add uniforms to existing program
+        if (receive && !mesh.program.uniforms[uniformProjection]) {
+            mesh.program.uniforms[uniformProjection] = {value: this.light.projectionMatrix};
+            mesh.program.uniforms[uniformView] = {value: this.light.viewMatrix};
+            mesh.program.uniforms[uniformTexture] = {value: this.target.texture};
+        }
+
+        if (!cast) return;
+        this.castMeshes.push(mesh);
+
+        // Store program for when switching between depth override
+        mesh.colorProgram = mesh.program;
+
+        // Check if depth program already attached
+        if (mesh.depthProgram) return;
+
+        // Use global depth override if nothing custom passed in
+        if (vertex === defaultVertex && fragment === defaultFragment) {
+            mesh.depthProgram = this.depthProgram;
+            return;
+        }
+
+        // Create custom override program
+        mesh.depthProgram = new Program(gl, {
+            vertex,
+            fragment,
+            cullFace: null,
+        });
+    }
+
+    render({scene}) {
+
+        // For depth render, replace program with depth override.
+        // Hide meshes not casting shadows.
+        scene.traverse(node => {
+            if (!node.draw) return;
+            if (!!~this.castMeshes.indexOf(node)) {
+                node.program = node.depthProgram;
+            } else {
+                if (node.visible) node.isForceVisibility = true;
+                node.visible = false;
+            }
+        });
+
+        // Render the depth shadow map using the light as the camera
+        this.gl.renderer.render({
+            scene, 
+            camera: this.light, 
+            target: this.target,
+        });
+
+        // Then switch the program back to the normal one
+        scene.traverse(node => {
+            if (!node.draw) return;
+            if (!!~this.castMeshes.indexOf(node)) {
+                node.program = node.colorProgram;
+            } else {
+                if (node.isForceVisibility) node.visible = true;
+            }
+        });
+    }
+}
+
+const defaultVertex = `
+    attribute vec3 position;
+    attribute vec2 uv;
+
+    uniform mat4 modelViewMatrix;
+    uniform mat4 projectionMatrix;
+
+    void main() {
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    }
+`;
+
+const defaultFragment = `
+    precision highp float;
+
+    vec4 packRGBA (float v) {
+        vec4 pack = fract(vec4(1.0, 255.0, 65025.0, 16581375.0) * v);
+        pack -= pack.yzww * vec2(1.0 / 255.0, 0.0).xxxy;
+        return pack;
+    }
+
+    void main() {
+        gl_FragColor = packRGBA(gl_FragCoord.z);
+    }
+`;
