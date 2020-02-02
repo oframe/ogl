ecbdfdb43f89e1239cf4d252f30cbd8c0332266e
Joseph Merdrignac <joseph.merdrignac@gmail.com>
2020-02-02 17:47:18 +0100

    Fix orthographic raycast

--- src/extras/Raycast.js
+++ src/extras/Raycast.js
@@ -19,14 +19,29 @@ export class Raycast {
 
     // Set ray from mouse unprojection
     castMouse(camera, mouse = [0, 0]) {
+        if (camera.type === 'orthographic') {
+            // Set origin
+            // Since camera is orthographic, origin is not the camera position
+            const {left, right, bottom, top} = camera;
+            const x = left + (right - left) * (mouse[0] * .5 + .5);
+            const y = bottom + (top - bottom) * (mouse[1] * .5 + .5);
+            this.origin.set(x, y, 0);
+            this.origin.applyMatrix4(camera.worldMatrix);
 
-        // Set origin
-        camera.worldMatrix.getTranslation(this.origin);
-        
-        // Set direction
-        this.direction.set(mouse[0], mouse[1], 0.5);
-        camera.unproject(this.direction);
-        this.direction.sub(this.origin).normalize();
+            // Set direction
+            // https://community.khronos.org/t/get-direction-from-transformation-matrix-or-quat/65502/2
+            this.direction.x = -camera.worldMatrix[8];
+            this.direction.y = -camera.worldMatrix[9];
+            this.direction.z = -camera.worldMatrix[10];
+        } else {
+            // Set origin
+            camera.worldMatrix.getTranslation(this.origin);
+
+            // Set direction
+            this.direction.set(mouse[0], mouse[1], 0.5);
+            camera.unproject(this.direction);
+            this.direction.sub(this.origin).normalize();
+        }
     }
 
     intersectBounds(meshes) {
