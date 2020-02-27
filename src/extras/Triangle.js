c46e1d1fb97f7cb17c671a98260091b8ad6237ee
nshen <nshen121@gmail.com>
2020-02-27 17:02:40 +0800

    update base-primitives.html

--- src/extras/Triangle.js
+++ src/extras/Triangle.js
@@ -1,14 +1,16 @@
 import { Geometry } from '../core/Geometry.js';
 
 export class Triangle extends Geometry {
-    constructor(gl, { 
+    constructor(gl, {
         attributes = {},
     } = {}) {
 
         Object.assign(attributes, {
-            position: { size: 2, data: new Float32Array([-1, -1, 3, -1, -1, 3]) },
+            // position: { size: 2, data: new Float32Array([-1, -1, 3, -1, -1, 3]) },
+            position: { size: 3, data: new Float32Array([-1, -1, 0, 3, -1, 0, -1, 3, 0]) },
             uv: { size: 2, data: new Float32Array([0, 0, 2, 0, 0, 2]) },
-        }); 
+            normal: { size: 3, data: new Float32Array([0, 0, 1, 0, 0, 1, 0, 0, 1]) }
+        });
 
         super(gl, attributes);
     }
