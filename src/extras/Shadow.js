30caeb7491df803e8d0223548b9c210fe1ec1f29
月影 <akira.cn@gmail.com>
2020-03-18 19:09:25 +0800

    Fix set mesh visiblity after shadow init will be override by shadow render (#58)

--- src/extras/Shadow.js
+++ src/extras/Shadow.js
@@ -73,7 +73,7 @@ export class Shadow {
             if (!!~this.castMeshes.indexOf(node)) {
                 node.program = node.depthProgram;
             } else {
-                if (node.visible) node.isForceVisibility = true;
+                node.isForceVisibility = node.visible;
                 node.visible = false;
             }
         });
@@ -91,7 +91,7 @@ export class Shadow {
             if (!!~this.castMeshes.indexOf(node)) {
                 node.program = node.colorProgram;
             } else {
-                if (node.isForceVisibility) node.visible = true;
+                node.visible = node.isForceVisibility;
             }
         });
     }
