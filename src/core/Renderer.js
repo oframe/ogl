7e0ac56549ed586cb861230caee625ae44a53f92
Nathan Gordon <gordonnl@gmail.com>
2021-02-05 22:15:53 +0100

    add console error for no webgl support. Fixes #87

--- src/core/Renderer.js
+++ src/core/Renderer.js
@@ -45,6 +45,7 @@ export class Renderer {
         if (!this.gl) {
             this.gl = canvas.getContext('webgl', attributes) || canvas.getContext('experimental-webgl', attributes);
         }
+        if (!this.gl) console.error('unable to create webgl context');
 
         // Attach renderer to gl so that all classes have access to internal state functions
         this.gl.renderer = this;
