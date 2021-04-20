582b259a994edb566db43b28eb0cf70cff241b75
Nathan Gordon <gordonnl@gmail.com>
2021-04-20 03:49:37 +0200

    fix Box top and bottom plane indices

--- src/extras/Box.js
+++ src/extras/Box.js
@@ -20,100 +20,28 @@ export class Box extends Geometry {
 
         // left, right
         Plane.buildPlane(position, normal, uv, index, depth, height, width, dSegs, hSegs, 2, 1, 0, -1, -1, i, ii);
-        Plane.buildPlane(
-            position,
-            normal,
-            uv,
-            index,
-            depth,
-            height,
-            -width,
-            dSegs,
-            hSegs,
-            2,
-            1,
-            0,
-            1,
-            -1,
-            (i += (dSegs + 1) * (hSegs + 1)),
-            (ii += dSegs * hSegs)
-        );
+        i += (dSegs + 1) * (hSegs + 1);
+        ii += dSegs * hSegs;
+
+        Plane.buildPlane(position, normal, uv, index, depth, height, -width, dSegs, hSegs, 2, 1, 0, 1, -1, i, ii);
+        i += (dSegs + 1) * (hSegs + 1);
+        ii += dSegs * hSegs;
 
         // top, bottom
-        Plane.buildPlane(
-            position,
-            normal,
-            uv,
-            index,
-            width,
-            depth,
-            height,
-            dSegs,
-            hSegs,
-            0,
-            2,
-            1,
-            1,
-            1,
-            (i += (dSegs + 1) * (hSegs + 1)),
-            (ii += dSegs * hSegs)
-        );
-        Plane.buildPlane(
-            position,
-            normal,
-            uv,
-            index,
-            width,
-            depth,
-            -height,
-            dSegs,
-            hSegs,
-            0,
-            2,
-            1,
-            1,
-            -1,
-            (i += (wSegs + 1) * (dSegs + 1)),
-            (ii += wSegs * dSegs)
-        );
+        Plane.buildPlane(position, normal, uv, index, width, depth, height, dSegs, wSegs, 0, 2, 1, 1, 1, i, ii);
+        i += (wSegs + 1) * (dSegs + 1);
+        ii += wSegs * dSegs;
+
+        Plane.buildPlane(position, normal, uv, index, width, depth, -height, dSegs, wSegs, 0, 2, 1, 1, -1, i, ii);
+        i += (wSegs + 1) * (dSegs + 1);
+        ii += wSegs * dSegs;
 
         // front, back
-        Plane.buildPlane(
-            position,
-            normal,
-            uv,
-            index,
-            width,
-            height,
-            -depth,
-            wSegs,
-            hSegs,
-            0,
-            1,
-            2,
-            -1,
-            -1,
-            (i += (wSegs + 1) * (dSegs + 1)),
-            (ii += wSegs * dSegs)
-        );
-        Plane.buildPlane(
-            position,
-            normal,
-            uv,
-            index,
-            width,
-            height,
-            depth,
-            wSegs,
-            hSegs,
-            0,
-            1,
-            2,
-            1,
-            -1,
-            (i += (wSegs + 1) * (hSegs + 1)),
-            (ii += wSegs * hSegs)
-        );
+        Plane.buildPlane(position, normal, uv, index, width, height, -depth, wSegs, hSegs, 0, 1, 2, -1, -1, i, ii);
+        i += (wSegs + 1) * (hSegs + 1);
+        ii += wSegs * hSegs;
+
+        Plane.buildPlane(position, normal, uv, index, width, height, depth, wSegs, hSegs, 0, 1, 2, 1, -1, i, ii);
 
         Object.assign(attributes, {
             position: { size: 3, data: position },
