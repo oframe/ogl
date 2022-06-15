fcc57a66ad5bccd0832f452dcdeb0cea0415420a
RAX7 <antoxa2013@gmail.com>
2022-06-16 00:16:09 +0700

    WireMesh: Draw only unique edges

--- src/extras/WireMesh.js
+++ src/extras/WireMesh.js
@@ -11,35 +11,52 @@ export class WireMesh extends Mesh {
             uniforms: { wireColor: { value: wireColor } },
         });
 
-        let indicesArray;
+        const positionArray = geometry.attributes.position.data;
+        const indices = [];
+        const hashSet = new Set();
 
         if (geometry.attributes.index) {
-            const data = geometry.attributes.index.data;
-            const numIndices = (data.length / 3) * 6;
-            indicesArray = new data.constructor(numIndices);
+            const idata = geometry.attributes.index.data;
 
-            for (let i = 0; i < data.length; i += 3) {
-                indicesArray.set([data[i], data[i + 1], data[i + 1], data[i + 2], data[i + 2], data[i]], i * 2);
+            for (let i = 0; i < idata.length; i += 3) {
+                const idxs = [idata[i], idata[i + 1], idata[i + 1], idata[i + 2], idata[i + 2], idata[i]];
+                for (let j = 0; j < idxs.length; j += 2) {
+                    if (isUniqueEdgePosition(idxs[j] * 3, idxs[j + 1] * 3, positionArray, hashSet)) {
+                        indices.push(idxs[j], idxs[j + 1]);
+                    }
+                }
             }
         } else {
-            // from: https://github.com/oframe/ogl/blob/fceff3a9bd4e2d31a1dca04491cdef11c6cae86e/examples/wireframe.html#L112
-            const numVertices = geometry.attributes.position.data.length / 3;
-            const numIndices = (numVertices / 3) * 6;
-            indicesArray = numIndices > 65536 ? new Uint32Array(numIndices) : new Uint16Array(numIndices);
+            const numVertices = Math.floor(positionArray.length / 3);
 
             for (let i = 0; i < numVertices; i += 3) {
-                // For every triangle, make three line pairs (start, end)
-                indicesArray.set([i, i + 1, i + 1, i + 2, i + 2, i], i * 2);
+                const idxs = [i, i + 1, i + 1, i + 2, i + 2, i];
+                for (let j = 0; j < idxs.length; j += 2) {
+                    if (isUniqueEdgePosition(idxs[j] * 3, idxs[j + 1] * 3, positionArray, hashSet)) {
+                        indices.push(idxs[j], idxs[j + 1]);
+                    }
+                }
             }
         }
 
+        const indicesTyped = indices.length > 65536 ? new Uint32Array(indices) : new Uint16Array(indices);
         const wireGeometry = new Geometry(gl, {
             position: { ...geometry.attributes.position },
-            index: { data: indicesArray },
+            index: { data: indicesTyped },
         });
 
         super(gl, { ...meshProps, mode: gl.LINES, geometry: wireGeometry, program: wireProgram });
     }
+}
+
+// from https://github.com/mrdoob/three.js/blob/0c26bb4bb8220126447c8373154ac045588441de/src/geometries/WireframeGeometry.js#L116
+function isUniqueEdgePosition(start, end, pos, hashSet) {
+    const hash1 = [pos[start], pos[start + 1], pos[start + 2], pos[end], pos[end + 1], pos[end + 2]].join('#');
+    const hash2 = [pos[end], pos[end + 1], pos[end + 2], pos[start], pos[start + 1], pos[start + 2]].join('#');
+    const oldSize = hashSet.size;
+    hashSet.add(hash1);
+    hashSet.add(hash2);
+    return hashSet.size - oldSize === 2;
 }
 
 const vertex = /* glsl */ `
