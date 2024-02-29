d195089f47feace8a8564fe434bf2cdce4151e37
Nathan Gordon <gordonnl@gmail.com>
2024-02-29 01:45:21 +0100

    fix rotation-quaternion sync when manipulating array elements

--- src/math/Euler.js
+++ src/math/Euler.js
@@ -8,7 +8,19 @@ export class Euler extends Array {
         super(x, y, z);
         this.order = order;
         this.onChange = () => {};
-        return this;
+
+        // Keep reference to proxy target to avoid triggering onChange internally
+        this._target = this;
+
+        // Return a proxy to trigger onChange when array elements are edited directly
+        const triggerProps = ['0', '1', '2'];
+        return new Proxy(this, {
+            set(target, property) {
+                const success = Reflect.set(...arguments);
+                if (success && triggerProps.includes(property)) target.onChange();
+                return success;
+            },
+        });
     }
 
     get x() {
@@ -24,58 +36,61 @@ export class Euler extends Array {
     }
 
     set x(v) {
-        this[0] = v;
+        this._target[0] = v;
         this.onChange();
     }
 
     set y(v) {
-        this[1] = v;
+        this._target[1] = v;
         this.onChange();
     }
 
     set z(v) {
-        this[2] = v;
+        this._target[2] = v;
         this.onChange();
     }
 
     set(x, y = x, z = x) {
         if (x.length) return this.copy(x);
-        this[0] = x;
-        this[1] = y;
-        this[2] = z;
+        this._target[0] = x;
+        this._target[1] = y;
+        this._target[2] = z;
         this.onChange();
         return this;
     }
 
     copy(v) {
-        this[0] = v[0];
-        this[1] = v[1];
-        this[2] = v[2];
+        this._target[0] = v[0];
+        this._target[1] = v[1];
+        this._target[2] = v[2];
         this.onChange();
         return this;
     }
 
     reorder(order) {
-        this.order = order;
+        this._target.order = order;
         this.onChange();
         return this;
     }
 
     fromRotationMatrix(m, order = this.order) {
-        EulerFunc.fromRotationMatrix(this, m, order);
+        EulerFunc.fromRotationMatrix(this._target, m, order);
         this.onChange();
         return this;
     }
 
-    fromQuaternion(q, order = this.order) {
+    fromQuaternion(q, order = this.order, isInternal) {
         tmpMat4.fromQuaternion(q);
-        return this.fromRotationMatrix(tmpMat4, order);
+        this._target.fromRotationMatrix(tmpMat4, order);
+        // Avoid infinite recursion
+        if (!isInternal) this.onChange();
+        return this;
     }
 
     fromArray(a, o = 0) {
-        this[0] = a[o];
-        this[1] = a[o + 1];
-        this[2] = a[o + 2];
+        this._target[0] = a[o];
+        this._target[1] = a[o + 1];
+        this._target[2] = a[o + 2];
         return this;
     }
 
